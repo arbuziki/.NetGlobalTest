2851aa7ab05e798539f7db2addbf531b0e4d1a53
arbuziki <parfenovee@mail.ru>
2017-12-08 14:33:54 +0300

    1

---
+++ EF/EntityContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class EntityContext : DbContext
+    {
+
+    }
+}
